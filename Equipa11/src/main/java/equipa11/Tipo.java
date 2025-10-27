eca25fa9aaeab6022f5a00516dc62257ce88f869
Diogo <Diogo@10.179.16.181>
2025-10-27 16:57:00 +0000

    alteraçao classe Tipo

--- Equipa11/src/main/java/equipa11/Tipo.java
+++ Equipa11/src/main/java/equipa11/Tipo.java
@@ -1,14 +1,36 @@
 /**
- * 
+ * @author Diogo
  */
 package equipa11;
 import java.util.ArrayList;
+import jakarta.persistence.*;
 
 /**
  * 
  */
+@Entity
+@Table (name = "Tipo")
 public class Tipo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idTipo;//PK
 	private String descriçaoTipo;
 	ArrayList<Parceiros> tipoParceiros = new ArrayList<>();
 	ArrayList<ProgramaVoluntariado> tipoPV = new ArrayList<>();
+	
+	public Tipo () {}
+
+	public Tipo(long idTipo, String descriçaoTipo) {
+		this.idTipo = idTipo;
+		this.descriçaoTipo = descriçaoTipo;
+		}
+
+	public long getIdTipo() {
+		return idTipo;
+	}
+
+	public String getDescriçaoTipo() {
+		return descriçaoTipo;
+	}
+	
 }
