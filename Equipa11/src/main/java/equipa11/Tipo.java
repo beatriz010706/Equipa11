dcb64480dda43e91d14dce4338a05278b8bd7836
miguel <imim18032000@gmail.com>
2025-10-28 12:37:32 +0000

    correçao de erros

--- Equipa11/src/main/java/equipa11/Tipo.java
+++ Equipa11/src/main/java/equipa11/Tipo.java
@@ -1,14 +1,51 @@
 /**
- * 
+ * @author diogo
  */
 package equipa11;
-import java.util.ArrayList;
-
+import java.util.ArrayList; 
+import jakarta.persistence.*;
 /**
  * 
  */
+@Entity
+@Table (name = "Tipo")
 public class Tipo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idTipo;//PK
 	private String descriçaoTipo;
 	ArrayList<Parceiro> tipoParceiros = new ArrayList<>();
 	ArrayList<ProgramaVoluntariado> tipoPV = new ArrayList<>();
+	
+	public Tipo () {}
+
+	public Tipo(long idTipo, String descriçaoTipo) {
+		this.idTipo = idTipo;
+		this.descriçaoTipo = descriçaoTipo;
+		}
+
+	public long getIdTipo() {
+		return idTipo;
+	}
+	
+	
+
+	public void setDescriçaoTipo(String descriçaoTipo) {
+		this.descriçaoTipo = descriçaoTipo;
+	}
+
+	public String getDescriçaoTipo() {
+		return descriçaoTipo;
+	}
+
+	public ArrayList<Parceiro> getTipoParceiros() {
+		return tipoParceiros;
+	}
+
+	public void setTipoParceiros(ArrayList<Parceiro> tipoParceiros) {
+		this.tipoParceiros = tipoParceiros;
+	}
+	
+	
+	
 }
